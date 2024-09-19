5c62fd3c7d36cf396cdcae143f0396bd72999339
sanskar <sanskargupta37081@gmail.com>
2024-09-19 14:35:18 +0530

    changes in data board.ts

--- src/data/board.ts
+++ src/data/board.ts
@@ -1,30 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 import taskImage from "../assets/images/task.jpg";
-import taskImage2 from "../assets/images/task2.jpg";
+//import taskImage2 from "../assets/images/task2.jpg";
 import taskImage3 from "../assets/images/task3.jpg";
 import { Columns } from "../types";
 import { getRandomColors } from "../helpers/getRandomColors";
 
 export const Board: Columns = {
-	backlog: {
-		name: "Backlog",
-		items: [
-			{
-				id: uuidv4(),
-				title: "Frontend Functionality to add task",
-				description: "Learnt Something New",
-				priority: "medium",
-				deadline: 50,
-				image: taskImage2,
-				alt: "task image",
-				tags: [
-					{ title: "Test", ...getRandomColors() },
-					{ title: "Front", ...getRandomColors() },
-				],
-			},
-			
-		],
-	},
+	
 	pending: {
 		name: "Pending",
 		items: [
@@ -76,7 +58,7 @@ export const Board: Columns = {
 		],
 	},
 	done: {
-		name: "Done",
+		name: "Completed",
 		items: [
 			{
 				id: uuidv4(),
